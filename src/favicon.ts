d6fb2ddde688f6e16fbc1a4f18782c177738cc58
Ryan Rutkin <ryan.rutkin@hubinternational.com>
2025-10-07 13:09:10 -0500

    Can now be initialized with Image

--- src/favicon.ts
+++ src/favicon.ts
@@ -4,25 +4,59 @@ import Png from "./png.js";
 import Resize from "./resize.js";
 
 class FaviconJS {
-  constructor(canvas: HTMLCanvasElement) {
-    this._canvas = canvas;
+  constructor(elem: HTMLCanvasElement | HTMLImageElement) {
+    if (elem instanceof HTMLImageElement) {
+      this._initializedWithImage = true;
+      this._image = elem;
+      this._canvas = this._convertImageToCanvas();
+    } else {
+      this._canvas = elem;
+    }
   }
 
   private _canvas: HTMLCanvasElement;
+  private _image: HTMLImageElement | null = null;
+  private _initializedWithImage: boolean = false;
+
+  private _convertImageToCanvas(): HTMLCanvasElement {
+    const width = this._image!.width;
+    const height = this._image!.height;
+    console.log(`FaviconJS initialized with image - width: ${width}; height: ${height}`);
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+      console.error('Unable to use FaviconJS. DOM restrictions in place');
+      return canvas;
+    }
+    ctx.drawImage(this._image!, 0, 0);
+    return canvas;
+  }
 
   bundle() {
+    if (this._initializedWithImage) {
+      this._canvas = this._convertImageToCanvas();
+    }
     return new Bundle(this._canvas).generate();
   }
 
   ico(sizes) {
+    if (this._initializedWithImage) {
+      this._canvas = this._convertImageToCanvas();
+    }
     return new Ico(this._canvas).generate(sizes);
   }
 
   png(size) {
+    if (this._initializedWithImage) {
+      this._canvas = this._convertImageToCanvas();
+    }
     return new Png(this._canvas).generate(size);
   }
 
   resize(size) {
+    if (this._initializedWithImage) {
+      this._canvas = this._convertImageToCanvas();
+    }
     return new Resize(this._canvas).generate(size, size);
   }
 }
